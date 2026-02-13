10e69de5b26ac0b74432193c812f515048f384b8
Ben Titran <bltitran65@gmail.com>
2026-02-13 13:02:17 -0500

    modified create_geoopt_withhydrogens - implemented optional heteroatom screening for files - cleaned up .pdb generation for each fragment - next step is to append each ligand-residue combination with rdmolchemicalgraphs's CombineMol() and generate the final files

--- create_geoopt_withhydrogens.py
+++ create_geoopt_withhydrogens.py
@@ -128,7 +128,7 @@ def main():
     for i, res in enumerate(neighbor_residues, start=1):
         print(f"  {i}: {describe_residue(res)}")
     
-    plot_frag_chk = input("Would you like to create .pdb files for each ligand/neighbor interaction? [y/n]: ").lower()
+    plot_frag_chk = input("NOTE: Script is currently unequipped to model atoms with multiple letters in element designation (i.e. NA or CL) \nWould you like to create .pdb files for each ligand/neighbor interaction? [y/n]: ").lower()
     while plot_frag_chk != "y":
         if plot_frag_chk == "n":
             sys.exit()
@@ -138,43 +138,40 @@ def main():
     while include_hetatms_chk != "y" and include_hetatms_chk != "n":
         include_hetatms_chk = input("Invalid input. Please select yes [y] or no [n]: ")
     
-    #need to retool this later to actually remove heteroatoms!!
-    #for res in neighbor_residues:
-    #    if include_hetatms_chk == "n":
-    #       het_chk = (res.get_id()[0])
-    #       if het_chk == " ":
-    #           print(res.get_full_id()[3])
-    #    else:
-    #       print(res.get_full_id()[3])
-    
-    noHs_path = "C:\\Users\\bltit\\Desktop\\test\\"
-    store_atom_coords(neighbor_residues, noHs_path)
-    def addHs_resfrag(in_path, out_path):
+
+    if include_hetatms_chk == "n":
+        frag_tuple = tuple(res for res in neighbor_residues if res.get_id()[0] == " ")
+    else:
+       frag_tuple = neighbor_residues
+
+    addhs_chk = input("Add hydrogens to .pdb file? [y/n]: ").lower()
+    while addhs_chk != "y" and addhs_chk != "n":
+        addhs_chk = input("Invalid input. Please select yes [y] or no [n]: ")
+
+    main_path = "C:\\Users\\bltit\\Desktop\\test\\"
+    store_atom_coords(frag_tuple, main_path)
+    def addHs_resfrag(inp_path, outp_path):
         """
         Add hydrogens to each fragment .pdb file
         
         :param in_path: input folder path of .pdb files without hydrogens
         :param out_path: output folder path of .pdb files with hydrogens added
         """
-        for f in os.listdir(in_path):
+        for f in os.listdir(inp_path):
 
-            f_path = in_path + f
+            f_path = inp_path + f
             print(f_path)
             rdkit_struct = Chem.rdmolfiles.MolFromPDBFile(f_path,True,True,0,True)
             print(type(rdkit_struct))
-        #    conf = rdkit_struct.GetConformer()
-        #    #for atom in rdkit_struct:
-        #    #    pos =conf.GetAtomPosition(atom.GetIdx())
-        #    #    print(f"{atom.GetSymbol()}{atom.GetIdx()}: {pos.x}, {pos.y}, {pos.z}")
             with_hs = Chem.rdmolops.AddHs(rdkit_struct, addCoords=True, addResidueInfo=True)
             conf_withH = with_hs.GetConformer()
-        #    #for atom in with_hs.GetAtoms():
-        #    #    pos = conf_withH.GetAtomPosition(atom.GetIdx())
-        #    #    print(f"{atom.GetSymbol()}{atom.GetIdx()}: {pos.x}, {pos.y}, {pos.z}")
             f_out = f_path.removesuffix(".pdb") + "_HsAdded.pdb"
-            print(f_out)
             Chem.MolToPDBFile(with_hs,f_out, flavor = 2)
-    addHs_resfrag(noHs_path,noHs_path)
+            if os.path.exists(f_path):
+                os.remove(f_path)
+            else:
+                pass
+    addHs_resfrag(main_path,main_path)
 
 
 
